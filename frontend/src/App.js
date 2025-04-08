2086785a5b6c999f6e9b22c9a9fd9fdb6921a1b5
Ariel Diaz <arieldiaz.sistemas@gmail.composer>
2025-04-08 15:14:20 -0300

    create component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'; // Crearemos este componente
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import MovieDetailPage from './pages/MovieDetailPage';
+import AddMoviePage from './pages/AddMoviePage';
+import EditMoviePage from './pages/EditMoviePage';
+import AddReviewPage from './pages/AddReviewPage'; // ORO
+import ProtectedRoute from './components/ProtectedRoute';
+import NotFoundPage from './pages/NotFoundPage'; // Página para rutas no encontradas
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar /> {/* El Navbar se muestra en todas las páginas */}
+      <div className="container mt-4"> {/* Contenedor principal de Bootstrap */}
+        <Routes>
+          {/* Rutas Públicas */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+
+          {/* Rutas Protegidas */}
+          <Route element={<ProtectedRoute />}> {/* Envuelve las rutas protegidas */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies/new" element={<AddMoviePage />} />
+            <Route path="/movies/:id" element={<MovieDetailPage />} />
+            <Route path="/movies/:id/edit" element={<EditMoviePage />} />
+            {/* --- RUTAS ORO --- */}
+            <Route path="/movies/:id/reviews/new" element={<AddReviewPage />} />
+            {/* --- FIN RUTAS ORO --- */}
+          </Route>
+
+          {/* Ruta Catch-all para 404 */}
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
